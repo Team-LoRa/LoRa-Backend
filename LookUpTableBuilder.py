b8fecc71cd45a4a25aa40a11d74b2de2d4471f25
Brandon Salter <bss233@nau.edu>
2020-03-03 18:32:00 -0700

    updated lookup table

--- LookUpTableBuilder.py
+++ LookUpTableBuilder.py
@@ -5,33 +5,27 @@ import os
 def main(fileName):
     """ load json file specified by the 1st cli argument.
     Gets loaded as a standard python dict """
-    param_dict = {}
-    api_dict = {}
-    app_dict = {}
-    param_counter = 1
-    app_counter = 1
-    api_counter = 1
     with open(fileName) as devJSON:
         dictJSON = json.load(devJSON)
         # find the message types the dev submitted
         #testList = keyListBuilder(dictJSON)
+        app_counter = 1
+        app_dict = {}
         for (app_name, api_method_list) in dictJSON.items():
+            temp_api = {}
+            api_counter = 1
             for (api_method, param_list) in api_method_list.items():
-                print(param_list)
+                temp_params = {}
+                param_counter = 1
                 for param in param_list:
-                    param_dict[param_counter] = param
+                    temp_params[param_counter] = param
                     param_counter += 1
-                api_dict[api_counter] = api_method
+                temp_api[api_counter] = temp_params
                 api_counter += 1
-            app_dict[app_counter] = app_name
+            app_dict[app_counter] = temp_api
             app_counter += 1
-        '''print(app_dict)
-        print(api_dict)
-        print(param_dict) '''
-       
-def encode(name, upper_dict, count = 1):
-    pass
-
+    with open("output.json", 'w') as output:
+        json.dump(app_dict, output)  
 
 if __name__ == "__main__":
     main(sys.argv[1])
